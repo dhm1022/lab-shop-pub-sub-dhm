80bf17752e584cdb57b96e3753721e30af7b88a9
dhm1022 <117365912+dhm1022@users.noreply.github.com>
2022-12-22 14:34:40 +0900

    commit

---
+++ inventory/src/main/java/labshoppubsubdhm/domain/InventoryRepository.java
@@ -0,0 +1,10 @@
+package labshoppubsubdhm.domain;
+
+import labshoppubsubdhm.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="inventories", path="inventories")
+public interface InventoryRepository extends PagingAndSortingRepository<Inventory, Long>{
+
+}
---
+++ order/src/main/java/labshoppubsubdhm/domain/Order.java
@@ -0,0 +1,79 @@
+package labshoppubsubdhm.domain;
+
+import labshoppubsubdhm.domain.OrderPlaced;
+import labshoppubsubdhm.OrderApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+
+@Entity
+@Table(name="Order_table")
+@Data
+
+public class Order  {
+
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String productId;
+    
+    
+    
+    
+    
+    private Integer qty;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+    }
+    @PrePersist
+    public void onPrePersist(){
+        // Get request from Inventory
+        //labshoppubsubdhm.external.Inventory inventory =
+        //    Application.applicationContext.getBean(labshoppubsubdhm.external.InventoryService.class)
+        //    .getInventory(/** mapping value needed */);
+
+    }
+
+    public static OrderRepository repository(){
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+
+
+
+
+
+}
